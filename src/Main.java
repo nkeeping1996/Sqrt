2d9b55887ba2822f7e6d92351291bd1d15342c83
nkeeping1996 <nfk.keeping@gmail.com>
2022-09-21 09:27:26 -0400

    Still need to reduce time complexity

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int x = 8;
+        System.out.println(mySqrt(x));
+    }
+
+    public static int mySqrt(int x) {
+
+        if(x==2){
+            return 1;
+        }
+
+        for(int i = 1; i <= (x/2); i++){
+            int squared = i*i;
+            if(squared > x){
+                return i-1;
+            }
+            else if(squared == x){
+                return i;
+            }
+        }
+
+        return x;
+
     }
 }
